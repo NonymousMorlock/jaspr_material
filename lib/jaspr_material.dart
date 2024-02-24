f97e9062c54d02a357d1b71cbf539988d72b1806
NonymousMorlock <agamspaul@gmail.com>
2024-02-24 02:14:36 +0000

    ✨ Feature: Export

--- lib/jaspr_material.dart
+++ lib/jaspr_material.dart
@@ -1,4 +1,6 @@
 /// The material library for Jaspr
 library jaspr_material;
 
-export 'src/jaspr_material.dart';
+// export 'src/jaspr_material.dart';
+export 'src/icon.dart';
+export 'src/icons.dart';
