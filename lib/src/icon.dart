426ef91b48acb3bbc6bdb8e22cd49de41e55fa93
NonymousMorlock <agamspaul@gmail.com>
2025-10-25 13:15:56 +0000

    🧹 Refactor: Rename `colour` parameter to `color` in Icon component

--- lib/src/icon.dart
+++ lib/src/icon.dart
@@ -9,81 +9,81 @@ class Icon extends StatelessComponent {
   const Icon(
     this.iconData, {
     this.size,
-    this.colour,
+    this.color,
     this.lineHeight,
     this.styles,
-  })  : iconType = 'material-icons',
-        assert(
-          styles == null ||
-              (lineHeight == null && colour == null && size == null),
-          'If you are providing `styles` '
-          'then you have to provide the colour, size and lineHeight in the '
-          'styles instead of passing them as separate options',
-        );
+  }) : iconType = 'material-icons',
+       assert(
+         styles == null ||
+             (lineHeight == null && color == null && size == null),
+         'If you are providing `styles` '
+         'then you have to provide the color, size and lineHeight in the '
+         'styles instead of passing them as separate options',
+       );
 
   /// Constructs a `rounded` [Icon]
   const Icon.rounded(
     this.iconData, {
     this.size,
-    this.colour,
+    this.color,
     this.lineHeight,
     this.styles,
-  })  : iconType = 'material-icons-round',
-        assert(
-          styles == null ||
-              (lineHeight == null && colour == null && size == null),
-          'If you are providing `styles` '
-          'then you have to provide the colour, size and lineHeight in the '
-          'styles instead of passing them as separate options',
-        );
+  }) : iconType = 'material-icons-round',
+       assert(
+         styles == null ||
+             (lineHeight == null && color == null && size == null),
+         'If you are providing `styles` '
+         'then you have to provide the color, size and lineHeight in the '
+         'styles instead of passing them as separate options',
+       );
 
   /// Constructs a `sharp` [Icon]
   const Icon.sharp(
     this.iconData, {
     this.size,
-    this.colour,
+    this.color,
     this.lineHeight,
     this.styles,
-  })  : iconType = 'material-icons-sharp',
-        assert(
-          styles == null ||
-              (lineHeight == null && colour == null && size == null),
-          'If you are providing `styles` '
-          'then you have to provide the colour, size and lineHeight in the '
-          'styles instead of passing them as separate options',
-        );
+  }) : iconType = 'material-icons-sharp',
+       assert(
+         styles == null ||
+             (lineHeight == null && color == null && size == null),
+         'If you are providing `styles` '
+         'then you have to provide the color, size and lineHeight in the '
+         'styles instead of passing them as separate options',
+       );
 
   /// Constructs an `outlined` [Icon]
   const Icon.outlined(
     this.iconData, {
     this.size,
-    this.colour,
+    this.color,
     this.lineHeight,
     this.styles,
-  })  : iconType = 'material-icons-outlined',
-        assert(
-          styles == null ||
-              (lineHeight == null && colour == null && size == null),
-          'If you are providing `styles` '
-          'then you have to provide the colour, size and lineHeight in the '
-          'styles instead of passing them as separate options',
-        );
+  }) : iconType = 'material-icons-outlined',
+       assert(
+         styles == null ||
+             (lineHeight == null && color == null && size == null),
+         'If you are providing `styles` '
+         'then you have to provide the color, size and lineHeight in the '
+         'styles instead of passing them as separate options',
+       );
 
   /// Constructs a `two-tone` [Icon]
   const Icon.twoTone(
     this.iconData, {
     this.size,
-    this.colour,
+    this.color,
     this.lineHeight,
     this.styles,
-  })  : iconType = 'material-icons-two-tone',
-        assert(
-          styles == null ||
-              (lineHeight == null && colour == null && size == null),
-          'If you are providing `styles` '
-          'then you have to provide the colour, size and lineHeight in the '
-          'styles instead of passing them as separate options',
-        );
+  }) : iconType = 'material-icons-two-tone',
+       assert(
+         styles == null ||
+             (lineHeight == null && color == null && size == null),
+         'If you are providing `styles` '
+         'then you have to provide the color, size and lineHeight in the '
+         'styles instead of passing them as separate options',
+       );
 
   /// The style of this material icon.
   ///
@@ -101,7 +101,7 @@ class Icon extends StatelessComponent {
   final Unit? lineHeight;
 
   /// The colour of this icon.
-  final Color? colour;
+  final Color? color;
 
   /// The `style` attribute of this icon
   final Styles? styles;
@@ -115,13 +115,11 @@ class Icon extends StatelessComponent {
       color: color,
     );
 
-    return DomElement(
-      DomComponent(
-        tag: 'span',
-        classes: iconType,
-        attributes: {'style': iconStyle},
-        child: raw(iconData),
-      ),
+    return Component.element(
+      tag: 'span',
+      classes: iconType,
+      styles: Styles.combine([defaultStyle, ?styles]),
+      children: [raw(iconData)],
     );
   }
 }
